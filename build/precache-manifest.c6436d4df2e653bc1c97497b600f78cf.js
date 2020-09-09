3c3c14ba0fc24381b119ed58e7e801060a9e8888
unknown <faisykhan35@gmail.com>
2020-09-09 22:51:07 +0500

    C:/Program Files/Git/build commented in git ignore file

---
+++ build/precache-manifest.c6436d4df2e653bc1c97497b600f78cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679a186a9e0292af27bae3b941116dad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60a4b7dc2474f989d194",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7caf65eae1d0c67a9f28",
+    "url": "/static/css/main.764e6137.chunk.css"
+  },
+  {
+    "revision": "60a4b7dc2474f989d194",
+    "url": "/static/js/2.a6c5f8ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6c5f8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7caf65eae1d0c67a9f28",
+    "url": "/static/js/main.995cc4ab.chunk.js"
+  },
+  {
+    "revision": "4b755a2c829993950dc6",
+    "url": "/static/js/runtime-main.be1ef0b9.js"
+  }
+]);
